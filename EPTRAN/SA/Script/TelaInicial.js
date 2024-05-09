3477dd94727fd9e5d4162e4dd27ac3d3816b2418
johann_bauer <112898573+johann-sebastian-bauer@users.noreply.github.com>
2024-05-09 12:46:48 -0300

    Add files via upload
    wfjrteir6h67

--- EPTRAN/SA/Script/TelaInicial.js
+++ EPTRAN/SA/Script/TelaInicial.js
@@ -1,15 +1,21 @@
 var menu = document.getElementById("menu");
 var popUp = document.getElementById("menuHidden");
-var opac = document.getElementById("opacBlack");
+var opac = document.getElementById("opacBlack").classList;
 
 menu.addEventListener('click', function(){
     if(popUp.style.display == "none"){
     popUp.style.display = "block";
-    opac.style.display = "block";
+    opac.add("opacBlack");
     }
     else{
         popUp.style.display = "none";
-        opac.style.display = "none";
+        opac.remove("opacBlack");
     }
 })
+
+let subMenu = document.getElementById("subMenu");
+
+function toggleMenu(){
+    subMenu.classList.toggle("open-menu");
+}
 
