b0de3eac52288eb09de6a64c6e28ad52e1943f97
Josefh21 <160614764+Josefh21@users.noreply.github.com>
2024-05-21 12:46:26 -0300

    Add files via upload

--- EPTRAN/SA/Script/TelaInicial.js
+++ EPTRAN/SA/Script/TelaInicial.js
@@ -1,6 +1,16 @@
 var menu = document.getElementById("menu");
 var popUp = document.getElementById("miniMenu").classList;
 var opac = document.getElementById("opacBlack").classList;
+var elements = Array.from(document.getElementsByClassName('ativ'));
+var ativColor = ['rgb(114, 142, 58)','rgb(215, 60, 74)', 'rgb(100, 45, 110)', 'rgb(254, 188, 3)' ]
+var i = 0;
+elements.forEach((atv) => {
+if(i > 3){
+    i = 0;
+}
+atv.style.backgroundColor = ativColor[i]
+i++
+});
 
 menu.addEventListener('click', function(){
     popUp.remove("menuHidden");
@@ -12,7 +22,10 @@ menu.addEventListener('click', function(){
     opac.add("opacHide")
     }
     else{
-        popUp.remove("menuHide");
+        popUp.remove ("menuHide");
         opac.remove("opacHide")
     }
 })
+
+
+
