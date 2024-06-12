ba4325de869c345d8ec0719efbb1a7dd82227575
johann-sebastian-bauer <johannsebastianbauer@gmail.com>
2024-06-12 12:44:15 -0300

    foi criado a lógica e a aparência dos botões das atividades

--- SA/Script/TelaInicial.js
+++ SA/Script/TelaInicial.js
@@ -2,16 +2,24 @@ var menu = document.getElementById("menu");
 var popUp = document.getElementById("miniMenu").classList;
 var opac = document.getElementById("opacBlack").classList;
 var elements = Array.from(document.getElementsByClassName('ativ'));
-var ativColor = ['rgb(114, 142, 58)','rgb(215, 60, 74)', 'rgb(100, 45, 110)', 'rgb(254, 188, 3)' ]
-var i = 0;
-elements.forEach((atv) => {
-if(i > 3){
-    i = 0;
+var buttonsElements = Array.from(document.getElementsByClassName('ativ-button'));
+var ativColor = ['rgb(114, 142, 58)','rgb(215, 60, 74)', 'rgb(100, 45, 110)', 'rgb(254, 188, 3)' ];
+var buttonColor = ['rgba(72,87,40,255)', 'rgba(137,39,47,255)','rgba(59,35,64,255)', 'rgba(146,109,0,255)'];
+var a = 0;
+console.log(buttonsElements)
+console.log(elements)
+for(let i = 0; i < elements.length; i++){
+    if(a > 3){
+    a = 0;
+    }
+    elements[i].style.backgroundColor = ativColor[a]
+    console.log(buttonsElements[i])
+    if(buttonsElements[i]){
+    buttonsElements[i].style.backgroundColor = buttonColor[a]
 }
-atv.style.backgroundColor = ativColor[i]
-i++
-});
-
+    a++
+}
+    // 
 menu.addEventListener('click', function(){
     popUp.remove("menuHidden");
     popUp.toggle("menuShow");
@@ -28,6 +36,3 @@ menu.addEventListener('click', function(){
         document.body.style.overflow = "hidden"
     }
 })
-
-
-
