ae5136fef036f44e5d71fa45790809400bcd640c
johann-sebastian-bauer <johannsebastianbauer@gmail.com>
2024-07-01 11:58:46 -0300

    corrigi as variaveis

--- SA/Script/TelaDeCadastro1.js
+++ SA/Script/TelaDeCadastro1.js
@@ -1,9 +1,9 @@
-var sexoadois = document.getElementById("button");
+var botao = document.getElementById("button");
 
-sexoadois.addEventListener('click', function(e){
+botao.addEventListener('click', function(e){
     e.preventDefault
     
-    const triliche = setTimeout(function(){
+    setTimeout(function(){
         window.location.href = "//127.0.0.1:5500/SA/P%C3%A1ginas/TelaDeCadastro2.html"
     }, 2000)
 })
