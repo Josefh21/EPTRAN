96df121077777d6a8ce2fdc12b25064d796a4592
johann-sebastian-bauer <johannsebastianbauer@gmail.com>
2024-07-08 12:50:27 -0300

    modal na tela inicial

--- SA/Script/TelaInicial.js
+++ SA/Script/TelaInicial.js
@@ -52,3 +52,35 @@ menu.addEventListener('click', function(){
         document.body.style.overflow = "hidden"
     }
 })
+
+
+
+
+
+
+
+
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+  document.body.style.overflow = "hidden"
+}
+
+// When the user clicks on <span> (x), close the modal
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    console.log('aaaa')
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
